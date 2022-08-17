ae05910d5c299cd273eeffbd45ab318045b57e08
Harrion Putt <putth@macstudent.com.au>
2022-08-17 14:20:10 +1000

    Created All Blacks Game file

---
+++ Guess-A/All Blacks Player/AllBlacksGame.swift
@@ -0,0 +1,20 @@
+//
+//  AllBlacksGame.swift
+//  Guess-A
+//
+//  Created by Harrison Putt on 17/8/2022.
+//
+
+import SwiftUI
+
+struct AllBlacksGame: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AllBlacksGame_Previews: PreviewProvider {
+    static var previews: some View {
+        AllBlacksGame()
+    }
+}
