846121303e43e98351712f391f0de2a0625adb5a
Harrion Putt <putth@macstudent.com.au>
2022-08-17 14:24:02 +1000

    Use the Pokemon difficulty selection as a scaffold for the All Blacks difficulty selection

--- Guess-A/All Blacks Player/AllBlacksLevels.swift
+++ Guess-A/All Blacks Player/AllBlacksLevels.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct AllBlacksLevels: View {
     var body: some View {
-        Text("Harry was here")
+        VStack {
+            VStack {
+                Text("GUESS")
+                    .font(.largeTitle)
+                    .padding(0.2)
+                Text("AN")
+                    .font(.largeTitle)
+                    .padding(0.2)
+                Text("ALL BLACK")
+                    .font(.largeTitle)
+                    .padding(0.2)
+            }
+            .padding()
+            VStack {
+                Text("Difficulty Selection")
+                RoundedRectangle(cornerRadius: 100, style: .continuous)
+                    .frame(width: 160, height: 1)
+                VStack {
+                    NavigationLink(destination: AllBlacksGame()) {
+                        Text("Easy")
+                            .foregroundColor(.black)
+                            .background(RoundedRectangle(cornerRadius: 18).fill(Color.green).frame(width: 165, height: 50))
+                    }
+                    .padding(20)
+                    NavigationLink(destination: AllBlacksGame()) {
+                        Text("Hard")
+                            .foregroundColor(.black)
+                            .background(RoundedRectangle(cornerRadius: 18).fill(Color.red).frame(width: 165, height: 50))
+                    }
+                    .padding(20)
+                }
+                .padding(25)
+            }
+            .padding(.vertical, 80)
+            
+        }
     }
 }
 
