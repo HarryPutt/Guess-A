e86aac2facad81f353cd72de33488783deae96c4
Harrion Putt <putth@macstudent.com.au>
2022-09-19 19:19:48 +1000

    Created the button for checking a guess but haven't gotten the actual button function working yet

--- Guess-A/All Blacks Player/AllBlacksGame.swift
+++ Guess-A/All Blacks Player/AllBlacksGame.swift
@@ -10,12 +10,20 @@ import SwiftUI
 struct AllBlacksGame: View {
     
     @State private var guess = ""
+    
     var body: some View {
         VStack{
-            TextField("Harry waz 'ere!", text: $guess)
+            TextField("Guess the player here", text: $guess)
                 .padding(5)
                 .border(.black, width: 2.0)
                 .padding(15)
+            Button("Check Guess"){
+                
+            }
+            .padding()
+            .border(Color.black)
+            .background(Color.black)
+            .foregroundColor(Color.white)
         }
     }
 }
