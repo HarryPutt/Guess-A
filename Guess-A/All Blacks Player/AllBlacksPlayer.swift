da824d984494b21e21ac444152f4d6090999f76c
Harrion Putt <putth@macstudent.com.au>
2022-09-20 11:34:53 +1000

    Added in all of the players names and photos in both the easy and hard array's

---
+++ Guess-A/All Blacks Player/AllBlacksPlayer.swift
@@ -0,0 +1,21 @@
+//
+//  AllBlacksPlayer.swift
+//  Guess-A
+//
+//  Created by Harrison Putt on 20/9/2022.
+//
+
+import Foundation
+
+struct easyArray {
+    let playersE: [String]
+}
+
+let playersEasy = easyArray(playersE: ["George Bower", "Codie Taylor", "Tyrel Lomax", "Scott Barrett", "Brodie Retallick", "Shannon Frizell", "Sam Cane","Ardie Savea","Aaron Smith", "Richie Mo’unga", "Caleb Clarke", "David Havili", "Rieko Ioane", "Will Jordan", "Jordie Barrett"])
+
+
+struct hardArray {
+    let playersH: [String]
+}
+
+let playersHard = hardArray(playersH: ["Tony Woodcock", "Sean Fitzpatrick", "Owen Franks", "Colin Meads", "Brodie Retallick", "Michael Jones", "Richie McCaw", "Zinzan Brooke", "Aaron Smith", "Dan Carter", "Jonah Lomu", "Ma’a Nonu", "Conrad Smith", "John Kirwan", "Christian Cullen"])
